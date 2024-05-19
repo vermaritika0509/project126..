462d39a8d78b719bc59b74e5fc137767f131da54
vermaritika0509 <97594978+vermaritika0509@users.noreply.github.com>
2024-05-19 15:41:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+song = '';
+
+function preload()
+{
+    song = loadsound('mouse-click-153941.mp3');
+    
+}
+
+scoreRightx = 0;
+scoreLefty = 0;
+
+RightwristX = 0;
+RightwristY = 0;
+
+LeftwristX = 0;
+RightwristY = 0;
+
+function setup()
+{
+    canvas = createCanvas(500 , 600);
+    canvas.create;
+
+    video = createCapture(VIDEO);
+    video.hide;
+
+    
+}
